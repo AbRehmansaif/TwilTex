0dd33d7e52e5f6b01d34b6d35a31435aad6e3b03
AbRehmanSaif <abdulrehmanarain713@gmail.com>
2026-02-16 02:03:05 +0500

    Add paginations

--- products/views.py
+++ products/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.core.paginator import Paginator
 from .models import Product
 
 def products_list(request):
-    """Products page with dynamic product cards"""
-    products = Product.objects.filter(is_active=True)
-    return render(request, 'products.html', {'products': products})
+    """Products page with dynamic product cards and pagination"""
+    products_list = Product.objects.filter(is_active=True).order_by('display_order', 'name')
+    paginator = Paginator(products_list, 9) # Show 9 products per page
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+    return render(request, 'products.html', {'page_obj': page_obj})
 
 def product_detail(request, slug):
     """Product detail page"""
